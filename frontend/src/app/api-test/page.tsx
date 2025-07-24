40222e831bfd9cdff545b6abd1c07124a650d48b
EmmaMaltseva <emmamaltseva0@gmail.com>
2025-07-24 23:16:32 +0300

    Создала next приложение с Tailwind; создала папку api-test с файлом page.tsx, сделала через useEffect и fetch отправку тестового запроса http://localhost:4000/api/hello, сделала вывод полученного ответа через data

---
+++ frontend/src/app/api-test/page.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+export default function ApiTestPage() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:4000/api/hello')
+      .then(res => res.json())
+      .then(data => setData(data.message));
+  }, []);
+
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-2xl font-bold mb-4">API Test</h1>
+      <p className="text-lg text-green-600">{data}</p>
+    </div>
+  );
+}
