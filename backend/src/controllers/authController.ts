bbd49d197468a7385c07bdc81b8d1ab8e2512ced
MaljEE <eemalceva@lotus.bank.srv>
2025-09-01 16:01:45 +0300

    в authController.ts в функции register сделала шифрование пароля и добавление пользователя в базу, сделала создание токена и отправку токена в куках ответа; в authController.ts добавила функцию login для проверки наличия пользователя и сравнения паролей (введенного и в базе), сделала создание токена и отправку токена в куках ответа; создала файл authRoutes.ts и прописала пути /register и /login для вызова соответствующих функций; в index.ts ипортировала authRoutes для прослушивания запросов

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -4,7 +4,6 @@ import jwt from 'jsonwebtoken';
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-
 const JWT_SECRET = process.env.JWT_SECRET || 'dev_secret';
 
 export const register = async (req: Request, res: Response) => {
@@ -14,8 +13,60 @@ export const register = async (req: Request, res: Response) => {
 
   try {
     const existingUser = await prisma.user.findUnique({ where: { email }});
+    if (existingUser) return res.status(400).json({ message: 'Пользователь уже существует'});
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: { email, password: hashedPassword },
+    });
+
+    const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: '7d' });
+
+    res
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: false, 
+        sameSite: 'lax',
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      })
+      .json({ message: 'Регистрация успешна', user: { id: user.id, email: user.email }})
+
   } catch (error) {
-    res.status(500).json({ message: 'Ошибка сервера'})
+    res.status(500).json({ message: 'Ошибка сервера'});
   }
 
 };
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({ where: { email }});
+    
+    if (!user) {
+      return res.status(401).json({ error: 'Неверный email или пароль' });
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+      return res.status(401).json({ error: 'Неверный email или пароль' });
+    }
+
+    const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: '1h' });
+
+    res
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: false, 
+        sameSite: 'lax',
+        maxAge: 60 * 60 * 1000, //1 час
+      })
+      .json({ message: 'Авторизация успешна' })
+
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Ошибка сервера'});
+    }
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import userRoutes from './routes/userRoutes';
+import authRoutes from './routes/authRoutes';
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cookieParser());
 
 app.use('/api', userRoutes);
+app.use('/api', authRoutes);
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Привет с бэкенда!'});
