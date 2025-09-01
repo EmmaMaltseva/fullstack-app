9a2034d2aefe0bdb4a084bf55db9981cfd1190a8
MaljEE <eemalceva@lotus.bank.srv>
2025-09-01 17:16:27 +0300

    установила библиотеку js-cookie; добавила комопнент AuthFrom.tsx - форма для регистрации и авторизации с запросами /register и /login; добавила страницу register для рг=егистрации

---
+++ frontend/src/app/register/page.tsx
@@ -0,0 +1,10 @@
+import AuthForm from "@components/AuthFrom";
+
+export default function RegisterPage() {
+  return (
+    <div>
+      <h1>Регистрация</h1>
+      <AuthForm type="register" />
+    </div>
+  )
+}
---
+++ frontend/src/components/AuthFrom.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function AuthForm({ type }: { type: 'login' | 'register'}) {
+  const router = useRouter();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError('');
+
+    const res = await fetch(`http://localhost:4000/api/${type}`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(
+        type === 'register' ? { name, email, password } : { email, password }
+      ),
+    });
+
+    if (res.ok) {
+      router.push('/dashboard');
+    } else {
+      const data = await res.json();
+      setError(data.error || 'Ошибка');
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      { type === 'register' && (
+        <input 
+          type="text" 
+          placeholder="Имя"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      )}
+      <input 
+          type="email" 
+          placeholder="Почта"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      <input 
+          type="password" 
+          placeholder="Пароль"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      { error && <p>{error}</p>}
+      <button
+        type="submit"
+      >
+        { type === 'login' ? 'Войти' : 'Зарегистрироваться' }
+      </button>
+    </form>
+  )
+}
