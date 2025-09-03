3ed6442148c0b34d8834a37932264de885f28a40
MaljEE <eemalceva@lotus.bank.srv>
2025-09-03 16:15:47 +0300

    стилизовала страницу регистрации, добавила шрифт Inter как основной

--- frontend/src/components/AuthFrom.tsx
+++ frontend/src/components/AuthFrom.tsx
@@ -36,6 +36,34 @@ export default function AuthForm({ type }: { type: "login" | "register" }) {
   return (
     <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
       <form onSubmit={handleSubmit} className="space-y-6">
+        <div>
+          <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">E-mail</label>
+          <div className="mt-2">
+            <input
+              id="email"
+              type="email"
+              placeholder="Почта"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+            />
+          </div>
+        </div>
+        <div>
+        <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">Пароль</label>
+        <div className="mt-2">
+          <input
+            id="password"
+            type="password"
+            placeholder="Пароль"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+          />
+        </div>
+        </div>
         {type === "register" && (
           <div>
             <label htmlFor="name" className="block text-sm/6 font-medium text-gray-900">Имя</label>
@@ -47,41 +75,15 @@ export default function AuthForm({ type }: { type: "login" | "register" }) {
                 value={name}
                 onChange={(e) => setName(e.target.value)}
                 required
-                className="w-full p-2 border rounded"
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
               />
             </div>
           </div>
         )}
-        <div>
-          <label className="block text-sm/6 font-medium text-gray-900">E-mail</label>
-          <div className="mt-2">
-            <input
-              type="email"
-              placeholder="Почта"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              className="w-full p-2 border rounded"
-            />
-          </div>
-        </div>
-        <div>
-        <label className="block text-sm/6 font-medium text-gray-900">Пароль</label>
-        <div className="mt-2">
-          <input
-            type="password"
-            placeholder="Пароль"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            className="w-full p-2 border rounded"
-          />
-        </div>
-        </div>
         {error && <p>{error}</p>}
         <button 
           type="submit"
-          className="bg-blue-600 text-white px-4 py-2 rounded"
+          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:text-sm/6"
         >
           {type === "login" ? "Войти" : "Зарегистрироваться"}
         </button>
